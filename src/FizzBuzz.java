a53b30ff07a416d347ae2ea18e010b129868aa7c
jiyapatel4 <jiya.patel@mail.utoronto.ca>
2023-09-12 12:28:22 -0400

    added a new class that calls the fizzbuzz method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -6,11 +6,11 @@ class FizzBuzz {
     public static void main(String[] args) {
         int i = 1;
         while (i < 100) {
-            i = doFizzBuss(i);
+            i = doFizzBuzz(i);
         }
     }
 
-    private static int doFizzBuss(int i) {
+    public static int doFizzBuzz(int i) {
         i++;
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+//        FizzBuzz.doFizzBuzz(5);
+//        Two fixes:
+//        1. pass a parameter of type String[]
+//        2. Remove the parameter from the method declaration in FizzBuzz.java
+        String[] empty = {};
+        FizzBuzz.main(empty);
+    }
+}
